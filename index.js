73eed1401d47fc58bc40a7427b59603a4547835c
Edy Segura <ednardo@inatel.br>
2017-11-27 23:52:09 -0200

    e2e integration

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 'use strict'
 
 const locale =  document.getElementById('locale')
+const currency = document.getElementById('currency')
+const date = document.getElementById('date')
 
-function showLocale (e) {
-    console.log(e.target.value)
+const inputNumber = 1234.56
+const inputDate = new Date()
+
+function getCurrency(locale) {
+    return {
+        'en-US': 'USD',
+        'pt-BR': 'BRL',
+        'de-DE': 'EUR',
+        'ja-JP': 'JPY'
+    }[locale]
 }
 
-locale.addEventListener('change', showLocale)
+function setLocale(locale) {
+    currency.innerHTML = inputNumber.toLocaleString(locale, { style: 'currency', currency: getCurrency(locale) })
+    date.innerHTML = inputDate.toLocaleString(locale)
+}
+
+setLocale(locale.value)
+locale.addEventListener('change', (e) => setLocale(e.target.value))
